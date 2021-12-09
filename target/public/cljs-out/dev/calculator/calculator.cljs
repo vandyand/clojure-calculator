da01c35bf09dbd2435e491a10bce013da46e6800
vandyand <vandyand@gmail.com>
2021-12-09 16:05:38 -0500

    feat: add button click function

--- target/public/cljs-out/dev/calculator/calculator.cljs
+++ target/public/cljs-out/dev/calculator/calculator.cljs
@@ -2,7 +2,9 @@
 
 (def button-rows '(("C" "%" "^" "/") ("7" "8" "9" "x") ("4" "5" "6" "-") ("1" "2" "3" "+") ("+/-" "0" "." "=")))
 
-(defn button [body] [:div {:class "calculator-button"} body])
+(defn handle-click [e] (js/console.log e.target.innerText))
+
+(defn button [body] [:div {:class "calculator-button" :on-click (fn [e] (handle-click e))} body])
 
 (defn row [buttons-text] [:div {:class "calculator-row"} (for [button-text buttons-text] (button button-text))])
 
--- target/public/cljs-out/dev/calculator/calculator.js
+++ target/public/cljs-out/dev/calculator/calculator.js
@@ -2,29 +2,34 @@
 goog.provide('calculator.calculator');
 goog.require('cljs.core');
 calculator.calculator.button_rows = cljs.core.list(cljs.core.list("C","%","^","/"),cljs.core.list("7","8","9","x"),cljs.core.list("4","5","6","-"),cljs.core.list("1","2","3","+"),cljs.core.list("+/-","0",".","="));
+calculator.calculator.handle_click = (function calculator$calculator$handle_click(e){
+return console.log(e.target.innerText);
+});
 calculator.calculator.button = (function calculator$calculator$button(body){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-button"], null),body], null);
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-button",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (e){
+return calculator.calculator.handle_click.call(null,e);
+})], null),body], null);
 });
 calculator.calculator.row = (function calculator$calculator$row(buttons_text){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-row"], null),(function (){var iter__4529__auto__ = (function calculator$calculator$row_$_iter__16783(s__16784){
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-row"], null),(function (){var iter__4529__auto__ = (function calculator$calculator$row_$_iter__16100(s__16101){
 return (new cljs.core.LazySeq(null,(function (){
-var s__16784__$1 = s__16784;
+var s__16101__$1 = s__16101;
 while(true){
-var temp__5735__auto__ = cljs.core.seq.call(null,s__16784__$1);
+var temp__5735__auto__ = cljs.core.seq.call(null,s__16101__$1);
 if(temp__5735__auto__){
-var s__16784__$2 = temp__5735__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__16784__$2)){
-var c__4527__auto__ = cljs.core.chunk_first.call(null,s__16784__$2);
+var s__16101__$2 = temp__5735__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__16101__$2)){
+var c__4527__auto__ = cljs.core.chunk_first.call(null,s__16101__$2);
 var size__4528__auto__ = cljs.core.count.call(null,c__4527__auto__);
-var b__16786 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
-if((function (){var i__16785 = (0);
+var b__16103 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
+if((function (){var i__16102 = (0);
 while(true){
-if((i__16785 < size__4528__auto__)){
-var button_text = cljs.core._nth.call(null,c__4527__auto__,i__16785);
-cljs.core.chunk_append.call(null,b__16786,calculator.calculator.button.call(null,button_text));
+if((i__16102 < size__4528__auto__)){
+var button_text = cljs.core._nth.call(null,c__4527__auto__,i__16102);
+cljs.core.chunk_append.call(null,b__16103,calculator.calculator.button.call(null,button_text));
 
-var G__16787 = (i__16785 + (1));
-i__16785 = G__16787;
+var G__16104 = (i__16102 + (1));
+i__16102 = G__16104;
 continue;
 } else {
 return true;
@@ -32,13 +37,13 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16786),calculator$calculator$row_$_iter__16783.call(null,cljs.core.chunk_rest.call(null,s__16784__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16103),calculator$calculator$row_$_iter__16100.call(null,cljs.core.chunk_rest.call(null,s__16101__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16786),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16103),null);
 }
 } else {
-var button_text = cljs.core.first.call(null,s__16784__$2);
-return cljs.core.cons.call(null,calculator.calculator.button.call(null,button_text),calculator$calculator$row_$_iter__16783.call(null,cljs.core.rest.call(null,s__16784__$2)));
+var button_text = cljs.core.first.call(null,s__16101__$2);
+return cljs.core.cons.call(null,calculator.calculator.button.call(null,button_text),calculator$calculator$row_$_iter__16100.call(null,cljs.core.rest.call(null,s__16101__$2)));
 }
 } else {
 return null;
@@ -54,25 +59,25 @@ calculator.calculator.display_bar = (function calculator$calculator$display_bar(
 return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-display-bar"], null)], null);
 });
 calculator.calculator.calculator = (function calculator$calculator$calculator(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-base"], null),cljs.core.cons.call(null,calculator.calculator.display_bar.call(null),(function (){var iter__4529__auto__ = (function calculator$calculator$calculator_$_iter__16788(s__16789){
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"calculator-base"], null),cljs.core.cons.call(null,calculator.calculator.display_bar.call(null),(function (){var iter__4529__auto__ = (function calculator$calculator$calculator_$_iter__16105(s__16106){
 return (new cljs.core.LazySeq(null,(function (){
-var s__16789__$1 = s__16789;
+var s__16106__$1 = s__16106;
 while(true){
-var temp__5735__auto__ = cljs.core.seq.call(null,s__16789__$1);
+var temp__5735__auto__ = cljs.core.seq.call(null,s__16106__$1);
 if(temp__5735__auto__){
-var s__16789__$2 = temp__5735__auto__;
-if(cljs.core.chunked_seq_QMARK_.call(null,s__16789__$2)){
-var c__4527__auto__ = cljs.core.chunk_first.call(null,s__16789__$2);
+var s__16106__$2 = temp__5735__auto__;
+if(cljs.core.chunked_seq_QMARK_.call(null,s__16106__$2)){
+var c__4527__auto__ = cljs.core.chunk_first.call(null,s__16106__$2);
 var size__4528__auto__ = cljs.core.count.call(null,c__4527__auto__);
-var b__16791 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
-if((function (){var i__16790 = (0);
+var b__16108 = cljs.core.chunk_buffer.call(null,size__4528__auto__);
+if((function (){var i__16107 = (0);
 while(true){
-if((i__16790 < size__4528__auto__)){
-var button_row = cljs.core._nth.call(null,c__4527__auto__,i__16790);
-cljs.core.chunk_append.call(null,b__16791,calculator.calculator.row.call(null,button_row));
+if((i__16107 < size__4528__auto__)){
+var button_row = cljs.core._nth.call(null,c__4527__auto__,i__16107);
+cljs.core.chunk_append.call(null,b__16108,calculator.calculator.row.call(null,button_row));
 
-var G__16792 = (i__16790 + (1));
-i__16790 = G__16792;
+var G__16109 = (i__16107 + (1));
+i__16107 = G__16109;
 continue;
 } else {
 return true;
@@ -80,13 +85,13 @@ return true;
 break;
 }
 })()){
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16791),calculator$calculator$calculator_$_iter__16788.call(null,cljs.core.chunk_rest.call(null,s__16789__$2)));
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16108),calculator$calculator$calculator_$_iter__16105.call(null,cljs.core.chunk_rest.call(null,s__16106__$2)));
 } else {
-return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16791),null);
+return cljs.core.chunk_cons.call(null,cljs.core.chunk.call(null,b__16108),null);
 }
 } else {
-var button_row = cljs.core.first.call(null,s__16789__$2);
-return cljs.core.cons.call(null,calculator.calculator.row.call(null,button_row),calculator$calculator$calculator_$_iter__16788.call(null,cljs.core.rest.call(null,s__16789__$2)));
+var button_row = cljs.core.first.call(null,s__16106__$2);
+return cljs.core.cons.call(null,calculator.calculator.row.call(null,button_row),calculator$calculator$calculator_$_iter__16105.call(null,cljs.core.rest.call(null,s__16106__$2)));
 }
 } else {
 return null;
